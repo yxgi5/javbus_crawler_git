750a332fa1f92bb1e6e1566e2b4151bebc4e3dbf
Andreas Zhang <denglitsch@gmail.com>
2024-11-27 11:58:26 +0800

    feat: 更新創建db邏輯，更新傳參數入口

--- controler.py
+++ controler.py
@@ -10,36 +10,51 @@ def _decode_utf8(aStr):
 
 def create_db():
     '''create a db and table if not exists'''
-    conn = sqlite3.connect("javbus.sqlite3.db")
-    cursor = conn.cursor()
+    
+    # conn = sqlite3.connect("javbus.sqlite3.db")
+    try:
+        conn = sqlite3.connect("file:javbus.sqlite3.db?mode=rw", uri=True)
 
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS JAVBUS_DATA(
-            URL       TEXT PRIMARY KEY,
-            識別碼    TEXT,
-            標題      TEXT,
-            封面      TEXT,
-            樣品圖像  TEXT, 
-            發行日期  TEXT,
-            長度      TEXT,
-            導演      TEXT,
-            製作商    TEXT,
-            發行商    TEXT,
-            系列      TEXT,
-            演員      TEXT,
-            類別      TEXT,
-            磁力链接  TEXT,
-            無碼      INTEGER);''')
+    except sqlite3.OperationalError:
+        # handle missing database case
+        conn = sqlite3.connect("javbus.sqlite3.db")
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS JAVBUS_DATA(
+                URL       TEXT PRIMARY KEY,
+                識別碼    TEXT,
+                標題      TEXT,
+                封面      TEXT,
+                樣品圖像  TEXT, 
+                發行日期  TEXT,
+                長度      TEXT,
+                導演      TEXT,
+                製作商    TEXT,
+                發行商    TEXT,
+                系列      TEXT,
+                演員      TEXT,
+                類別      TEXT,
+                磁力链接  TEXT,
+                無碼      INTEGER);''')
 
-    print("Table created successfully")
-    cursor.close()
-    conn.commit()
-    conn.close()
+        print("Table created successfully")
+        cursor.close()
+        conn.commit()
+        conn.close()
+
+    else:
+        conn.close()
 
 def write_data(dict_jav):
     '''write_data(dict_jav)'''
 
-    conn = sqlite3.connect("javbus.sqlite3.db")
+    # conn = sqlite3.connect("javbus.sqlite3.db")
+    try:
+        conn = sqlite3.connect("file:javbus.sqlite3.db?mode=rw", uri=True)
+    except sqlite3.OperationalError:
+        create_db()
+        conn = sqlite3.connect("file:javbus.sqlite3.db?mode=rw", uri=True)
+    
     cursor = conn.cursor()
     #对数据解码为unicode
     insert_data = list(map(_decode_utf8, (dict_jav['URL'], dict_jav['識別碼'], dict_jav['標題'], dict_jav['封面'], dict_jav['樣品圖像'], dict_jav['發行日期'], dict_jav['長度'], dict_jav['導演'], dict_jav['製作商'], dict_jav['發行商'], dict_jav['系列'], dict_jav['演員'], dict_jav['類別'], dict_jav['磁力链接'])))
